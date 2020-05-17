99f1281c53ee8306453d97255b3017b6bdd43401
Hampus <hwiren@kth.se>
2020-05-17 16:14:59 +0200

    ..

--- src/main/Start.java
+++ src/main/Start.java
@@ -2,9 +2,6 @@ package main;
 
 public class Start {
 
-	public Start() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
